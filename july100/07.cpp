190e5d228a871a8edf0009b3ac5a8b0e6760d236
ukeyim <ukeyim@gmail.com>
2016-12-29 23:52:29 +0800

    判断有环+搜索入口点

--- july100/07.cpp
+++ july100/07.cpp
@@ -11,14 +11,45 @@ class slist{
 	};
 }
 
+//判断有没有环,
 bool isExistLoop(slist *head){
 	slist *slow = head, *fast = head;
-	while(fast && fast->next != null){
+	while(fast && fast->next != null){ //为什么fast 和后面的节点同时不为null
 		slow = slow->next;
 		fast = fast->next->next;
 		if(slow == fast){
 			break;
 		}
 	}
-	return !(fast == NULL || fast->next == NULL)
+	return !(fast == NULL || fast->next == NULL) //return一个bool表达式更加高效一点
+}
+
+//判断入口
+slist* getEntry(slist *head){
+	slist *slow = head, *fast = head;
+	while(fast && fast->next != null){ //为什么fast 和后面的节点同时不为null
+		slow = slow->next;
+		fast = fast->next->next;
+		if(slow == fast){
+			break;
+		}
+	}
+	if(fast == NULL || fast->next == NULL){
+		return NULL;
+	} 
+	
+	slow = head;
+
+	// 我原来的代码真垃圾
+	// while(1){
+	while(slow != fast)
+		slow = slow->next;
+		fast = fast->next;
+		// if(slow == fast){
+		//     break;
+		// }
+	}
+	return slow;
+
+
 }
