e904f6749dd825acf8163a101c89e02aa74b2772
ukeyim <ukeyim@gmail.com>
2017-01-03 22:34:29 +0800

    max distance

---
+++ maxdistance.cpp
@@ -0,0 +1,16 @@
+int MaxDistance()
+{
+	if(_root == NULL) return 0;
+	int max = 0;
+	_MaxDistance( _root, max );
+	return max;
+}
+
+int _MaxDistance(Node * cur, int & max) //只返回深度
+{
+	if(cur== NULL) return 0;
+	int leftDepth = _MaxDistance(cur->left,max);
+	int rightDepth = _MaxDistance(cur->right,max);
+	max = leftDepth + rightDepth;
+	return leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;
+}
