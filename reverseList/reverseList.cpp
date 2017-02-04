a658efe9c02a73c38b7004be3bb3cc1ef03b6ee4
ukeyim <ukeyim@gmail.com>
2017-02-04 23:54:44 +0800

    add recursion way to reverse Linked list
    Signed-off-by: ukeyim <ukeyim@gmail.com>

--- reverseList/reverseList.cpp
+++ reverseList/reverseList.cpp
@@ -1,21 +1,69 @@
 //Reverse a linked list in two ways
 
-typedef struct Node
+typedef struct LNode
 {
 	int val;
-	struct Node *next;
-}Node;
+	struct LNode *next;
+}LNode, *LinkedList;
 
-int main()
+//Method 1, create a new linked list
+
+LinkedList ReverseList(LinkedList list)
 {
+	LinkedList newList;
+	LNode *tmp;
 
-	Node *p1,*p2,*p3;
-	p1 = root;
-	p2 = root->next;
-	p3 = p2.next;
+	if(list == NULL || (newList = (LinkedList)malloc(sizeof(LNode)))==NULL)
+	{
+		return NULL;
+	}
 
-	p2.next = p1;
-	p1 = p2;
-	p2 = p3;
-	p3 = p3.next;
+	newList->data = list->data;
+	newList->next = NULL;
+
+	while(list->next != NULL)
+	{
+		tmp = newList;
+		newList = list->next;
+		newList->next = tmp;
+		list->next = list->next->next;
+	}
+
+	free(list);
+	return newList;
+}
+
+Node *reverseList (List head)
+{
+	Node *pnow = head;
+	Node *pre = NULL;
+	Node *pnext = NULL;
+	Node *tail = NULL; //定义尾指针
+
+	while(pnow != NULL)
+	{
+		pnext = pnow->next;
+		if( pnext != NULL)
+		{
+			tail = pnow;
+		}
+		pnow->next = pre;
+		pre = pnow;
+		pnow = pnext; //往后一个，下一次循环时给pnext赋值往后一个
+	}
+	return tail;
+}
+
+//递归实现
+
+Node *reverseList(List head)
+{
+	if (head == NULL || head->next == NULL)
+	{
+		return head;
+	}
+	else
+	{
+
+	}
 }
